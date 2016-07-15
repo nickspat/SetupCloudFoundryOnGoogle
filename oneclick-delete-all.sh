0ba193e4d3220fa93565d4dd021185fb308e8730
Nikesh <nikeshbhai_n_patel@homedepot.com>
2016-07-15 15:29:40 -0500

    changing constants link

--- oneclick-delete-all.sh
+++ oneclick-delete-all.sh
@@ -4,7 +4,7 @@ set -e
 if [ -f ./constants.sh ]; then
     rm -rf ./constants.sh
 fi
-wget https://gist.github.com/nickspat/77430d2958e6b5a012674edb64dd8ed6/raw/ff6d2c369b595696f199d67e081a8ddf70e562d8/constants.sh
+wget https://gist.github.com/nickspat/77430d2958e6b5a012674edb64dd8ed6/raw/e70ca11bebba1d1de696f84e7d7a651c5da8f4ab/constants.sh
 chmod 744 ./constants.sh
 source ./constants.sh
 
