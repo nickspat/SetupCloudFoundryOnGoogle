64b5cc7ecef544ff404a4485846f7d33c23f5638
Nikesh <nikeshbhai_n_patel@homedepot.com>
2016-07-20 17:44:31 -0500

    going to create cf infra components before setting up bosh

--- all-setup.sh
+++ all-setup.sh
@@ -11,11 +11,11 @@ wget ${source_url}/constants.sh && chmod 744 ./constants.sh && source ./constant
 echo "-----------Setting up Infrastructure for BOSH director ----------------"
 wget ${source_url}/director-infra-setup.sh && chmod 744 ./director-infra-setup.sh && ./director-infra-setup.sh
 
-echo "-----------Setting up BOSH director ----------------"
-gcloud compute ssh bosh-bastion --zone ${google_zone} --command "wget ${source_url}/director-setup.sh && chmod 744 ./director-setup.sh && ./director-setup.sh"
-
 echo "-----------Setting up Infrastructure for Cloud Foundry ----------------"
 wget ${source_url}/cf-infra-setup.sh && chmod 744 ./cf-infra-setup.sh && ./cf-infra-setup.sh
+
+echo "-----------Setting up BOSH director ----------------"
+gcloud compute ssh bosh-bastion --zone ${google_zone} --command "wget ${source_url}/director-setup.sh && chmod 744 ./director-setup.sh && ./director-setup.sh"
 
 echo "----------Starting to setup Cloud Foundry components ------------------"
 ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/google_compute_engine `gcloud compute instances describe bosh-bastion --zone ${google_zone} | grep natIP: | cut -f2 -d :` 'wget ${source_url}/cf-setup.sh && chmod 744 ./cf-setup.sh && ./cf-setup.sh'
