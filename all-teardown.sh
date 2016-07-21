768b386c2cabd9f3c1b50fc772de2f1d5bfa2b3f
Nikesh <nikeshbhai_n_patel@homedepot.com>
2016-07-21 07:26:19 -0500

    fix for ssh command

--- all-teardown.sh
+++ all-teardown.sh
@@ -9,7 +9,9 @@ fi
 wget ${source_url}/constants.sh && chmod 744 ./constants.sh && source ./constants.sh
 
 echo "-------------- Starting to delete cloud foundry setup -----------------"
-ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/google_compute_engine `gcloud compute instances describe bosh-bastion --zone ${google_zone} | grep natIP: | cut -f2 -d :` 'wget ${source_url}/cf-teardown.sh && chmod 744 ./cf-teardown.sh && ./cf-teardown.sh'
+bosh_ip=`gcloud compute instances describe bosh-bastion --zone ${google_zone} | grep natIP: | cut -f2 -d :`
+command="wget ${source_url}/cf-teardown.sh && chmod 744 ./cf-teardown.sh && ./cf-teardown.sh"
+ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ${bosh_ip} ${command}
 
 echo "-------------- Starting to teardown Cloud Foundry infrastructure components ---------------------"
 wget ${source_url}/cf-infra-teardown.sh && chmod 744 ./cf-infra-teardown.sh && ./cf-infra-teardown.sh
