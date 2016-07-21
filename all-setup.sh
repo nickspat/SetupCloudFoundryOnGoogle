41148defc318a9e733c301f634586266ad3f15b4
Nikesh <nikeshbhai_n_patel@homedepot.com>
2016-07-21 07:33:25 -0500

    fix for ssh command

--- all-setup.sh
+++ all-setup.sh
@@ -17,7 +17,10 @@ wget ${source_url}/cf-infra-setup.sh && chmod 744 ./cf-infra-setup.sh && ./cf-in
 echo "-----------Setting up BOSH director ----------------"
 gcloud compute ssh bosh-bastion --zone ${google_zone} --command "wget ${source_url}/director-setup.sh && chmod 744 ./director-setup.sh && ./director-setup.sh"
 
+
 echo "----------Starting to setup Cloud Foundry components ------------------"
-ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/google_compute_engine `gcloud compute instances describe bosh-bastion --zone ${google_zone} | grep natIP: | cut -f2 -d :` 'wget ${source_url}/cf-setup.sh && chmod 744 ./cf-setup.sh && ./cf-setup.sh'
+bosh_ip=`gcloud compute instances describe bosh-bastion --zone ${google_zone} | grep natIP: | cut -f2 -d :`
+command="'wget ${source_url}/cf-setup.sh && chmod 744 ./cf-setup.sh && ./cf-setup.sh'"
+ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ${bosh_ip} 
 
 set -e
