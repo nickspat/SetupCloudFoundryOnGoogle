be041e160565083e274d2408108d13290d95a19f
Christopher <crgrant@gmail.com>
2016-07-26 18:22:56 -0500

    file name cleanup

--- all-setup.sh
+++ all-setup.sh
@@ -20,3 +20,6 @@ echo "-----------Setting up Cloud Foundry ----------------"
 bosh_ip=`gcloud compute instances describe bosh-bastion --zone ${google_zone} | grep natIP: | cut -f2 -d :`
 command="cd ./setupfiles && ./cf-setup.sh"
 ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ${bosh_ip} ${command}
+
+# drop you in the bastion server for convienence 
+gcloud compute ssh bosh-bastion --zone ${google_zone}
