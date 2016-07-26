9a30f1cd21105dbdddc07c21b194ba0a1e6f411a
Christopher <crgrant@gmail.com>
2016-07-26 13:13:41 -0500

    file name cleanup

--- all-setup-2.sh
+++ all-setup-2.sh
@@ -6,14 +6,14 @@ source ./constants.sh
 echo "-----------Setting up Infrastructure for BOSH director ----------------"
 ./director-infra-setup.sh
 
+echo "-----------Setting up Infrastructure for Cloud Foundry ----------------"
+./cf-infra-setup.sh
+
+
 ./sendfiles.sh
 
 echo "-----------Setting up BOSH director ----------------"
 gcloud compute ssh bosh-bastion --zone ${google_zone} --command "cd ./setupfiles && source ./constants.sh && ./director-setup.sh"
-
-
-echo "-----------Setting up Infrastructure for Cloud Foundry ----------------"
-gcloud compute ssh bosh-bastion --zone ${google_zone} --command "cd ./setupfiles && source ./constants.sh && ./cf-infra-setup.sh"
 
 
 echo "-----------Setting up Cloud Foundry ----------------"
