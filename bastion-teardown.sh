d667fe3cbd69ba6808d46da7ad8e4ef8a87f541d
Christopher <crgrant@gmail.com>
2016-07-26 10:23:32 -0500

    teardown steps

---
+++ bastion-teardown.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+source ./constants.sh
+echo "-------------- Starting to delete cloud foundry setup -----------------"
+./cf-teardown.sh
+
+echo "-------------- Starting to teardown Bosh Director -----------------"
+./director-teardown.sh
--- cf-teardown.sh
+++ cf-teardown.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
-set -e
-
-source_url="https://github.com/cgrant/setupcfongcp/raw/master"
-google_region="us-west1"
-google_zone=$google_region"-b"
-
-
-if [ -f ./constants.sh ]; then
-    rm -rf ./constants.sh
-fi
-wget ${source_url}/constants.sh && chmod 744 ./constants.sh && source ./constants.sh
+source ./constants.sh
 
 echo "Setting up bosh target"
 /usr/local/bin/bosh target ${director_ip}
 
 deployment_name="cf"
 
-echo "Starting to deploy cloudfoundry"
+echo "Starting to destroy cloudfoundry"
 /usr/local/bin/bosh delete deployment ${deployment_name}
 
 echo "Delete cloudfoundry successful"
-set -e
