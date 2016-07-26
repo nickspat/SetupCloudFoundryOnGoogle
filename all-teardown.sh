fd461b09dd8483d68c5d04f3a7050ef918bbc5cb
Christopher <crgrant@gmail.com>
2016-07-26 14:56:22 -0500

    file name cleanup

--- all-teardown.sh
+++ all-teardown.sh
@@ -2,7 +2,9 @@
 source ./constants.sh
 
 echo "-------------- Starting to delete cloud foundry setup -----------------"
-ssh bosh-bastion ./cf-teardown.sh
+bosh_ip=`gcloud compute instances describe bosh-bastion --zone ${google_zone} | grep natIP: | cut -f2 -d :`
+command="cd ./setupfiles && ./cf-teardown.sh"
+ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/google_compute_engine ${bosh_ip} ${command}
 
 echo "-------------- Starting to teardown Bosh Director -----------------"
 ssh bosh-bastion ./director-teardown.sh
